fee4d30e5dbdb552c003e7cc7fba49499959a02d
Stephen Romero <stephen.romero93@gmail.com>
2017-01-26 13:36:14 -0600

    textfields

--- Basic Swift App/ViewController.swift
+++ Basic Swift App/ViewController.swift
@@ -13,19 +13,19 @@ class ViewController: UIViewController {
     //Label that was added to the screen
     @IBOutlet weak var theLabel: UILabel!
     
+    @IBOutlet weak var textVar1: UITextField!
     
-    var tapCount = 0
+    @IBOutlet weak var textVar2: UITextField!
+    
     
     
     //action button
     @IBAction func buttonpressed(_ sender: Any) {
         
-        tapCount = tapCount + 1
-        print (tapCount)
+        print(textVar1.text!)
+        print(textVar2.text!)
         
-        if tapCount >= 20 {
-            theLabel.text = "You tapped the button 20 times!"
-        }
+        
     }
     //another action button
     @IBAction func extraButton(_ sender: Any) {
